dc37be37c4dad82849e922adaedc5e7a2858cf16
Kanhaiya <kanhaiyas@beelabs.technology>
2023-09-13 14:52:56 +0530

    PR Raised

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -138,3 +138,38 @@ export const DeliveryPartnersData = [
         link_url:""
     }
 ]
+
+export const Location = [
+    { id: 1, location_name: "", link_url: "/" },
+    { id: 2, location_name: "", link_url: "/" },
+    { id: 3, location_name: "", link_url: "/" },
+    { id: 4, location_name: "", link_url: "/" },
+    { id: 5, location_name: "", link_url: "/" },
+    { id: 6, location_name: "", link_url: "/" },
+    { id: 7, location_name: "", link_url: "/" },
+    { id: 8, location_name: "", link_url: "/" },
+    { id: 9, location_name: "", link_url: "/" },
+    { id: 10, location_name: "", link_url: "/" },
+    { id: 11, location_name: "", link_url: "/" },
+    { id: 12, location_name: "", link_url: "/" },
+    { id: 13, location_name: "", link_url: "/" },
+    { id: 14, location_name: "", link_url: "/" },
+    { id: 15, location_name: "", link_url: "/" },
+    { id: 16, location_name: "", link_url: "/" },
+    { id: 17, location_name: "", link_url: "/" },
+    { id: 18, location_name: "", link_url: "/" },
+    { id: 19, location_name: "", link_url: "/" },
+    { id: 20, location_name: "", link_url: "/" },
+    { id: 21, location_name: "", link_url: "/" },
+    { id: 22, location_name: "", link_url: "/" },
+    { id: 23, location_name: "", link_url: "/" },
+    { id: 24, location_name: "", link_url: "/" },
+    { id: 25, location_name: "", link_url: "/" },
+    { id: 26, location_name: "", link_url: "/" },
+    { id: 27, location_name: "", link_url: "/" },
+    { id: 28, location_name: "", link_url: "/" },
+    { id: 29, location_name: "", link_url: "/" },
+    { id: 30, location_name: "", link_url: "/" },
+    { id: 31, location_name: "", link_url: "/" },
+    
+]
