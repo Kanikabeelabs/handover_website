b80ca5d5798aafbe6aedb3b9e61aad03204ff890
Utsav <utsavd@beelabs.technology>
2023-09-14 13:12:14 +0530

    resolve conflict

--- src/Pages/Retailer.js
+++ src/Pages/Retailer.js
@@ -29,17 +29,11 @@ const Retailer = () => {
 
         }
         try {
-<<<<<<< HEAD
-            let response = await postRequest("http://3.108.127.83:3001/api/v1/contactus", formData);
-            alert("You have successfully submitted the form");
-            resetForm();
-=======
             let response = await postRequest("/api/v1/contactus", formData);
             if (response.status === 201) {
                 alert("You have successfully submitted the form");
                 resetForm();
             }
->>>>>>> fc55de6adc9584f71e629aecac86eba10b4d9993
         } catch (err) {
             console.log(err)
         }
@@ -67,18 +61,6 @@ const Retailer = () => {
     return (<section className='retailer-section'>
         <Container fluid className="p-0">
             <Header page_name="retailer" />
-<<<<<<< HEAD
-                {/* First Section */}
-                <Container fluid className="p-0" style={{minHeight:"800px"}}>
-                    <Container fluid  className="d-flex p-5 business_container-owner"   
-                    style={{
-                        backgroundImage: 'url("./businessowner.png")',
-                        backgroundSize: "cover",
-                        minHeight:"600px",position:"relative"}}>
-                        {/* <div className="contact-us-form-div" style={{position:"absolute",top:"100px"}} >
-                        <h1 className='fw-bold' style={{color:"#ffffff"}}>More Orders, <span style={{color:'#F6911E'}}>More Business Await You!</span></h1>
-                        <p style={{width:"300px",color:"#ffffff"}}>Our impressive delivery services help businesses get orders from both new & existing customers. Now, it’s your turn to gain a healthy business through us.</p> */}
-=======
             {/* First Section */}
             <Container fluid className="p-0" style={{ minHeight: "800px" }}>
                 <Container fluid className="d-flex justify-content-center p-3"
@@ -86,36 +68,9 @@ const Retailer = () => {
                     <div className="contact-us-form-div" style={{ position: "absolute", top: "100px" }} >
                         <h1 className='fw-bold' style={{ color: "#ffffff" }}>More Orders, <span style={{ color: '#F6911E' }}>More Business Await You!</span></h1>
                         <p style={{ width: "300px", color: "#ffffff" }}>Our impressive delivery services help businesses get orders from both new & existing customers. Now, it’s your turn to gain a healthy business through us.</p>
->>>>>>> fc55de6adc9584f71e629aecac86eba10b4d9993
                         <div className="handover-shadow-div p-3 d-flex  flex-wrap">
-                            {/* <div className='col-md-5 col-12 d-flex '>
+                            <div className='col-md-5 col-12 d-flex '>
                                 <img src="./contact_us.png" alt="ContactUs" width="100%" />
-<<<<<<< HEAD
-                            </div> */}
-                            <div className='col-12 d-flex flex-column p-3'>
-                                        <h5 className="mb-4">GET IN TOUCH</h5>
-                                        <Formik
-                                            initialValues={initialValues}
-                                            validationSchema={contactUsSchemaValidation}
-                                            onSubmit={handleSubmit}
-                                        >
-                                            {({ errors,values, setFieldValue,resetForm }) => (
-                                                <Form className=" d-flex flex-column  gap-3">
-                                                    <div className="row">
-                                                    <div className="d-flex flex-column col-md-6 col-12">
-                                                        <label htmlFor="option">Choose a option</label>
-                                                        <select name="option" 
-                                                            id="option" className='custom-form-input mt-2' 
-                                                            onChange={(event) => setFieldValue("option", event.target.value)}
-                                                            value={values.option}>
-                                                            <option value="">Choose an Option</option>
-                                                            <option value="delivery_partner">Delivery Partner</option>
-                                                            <option value="retailer">Retailer</option>
-                                                            <option value="customer">Customer</option>
-                                                            <option value="other">Other</option>
-                                                        </select>
-                                                        {errors.option && <div className="form-error">{errors.option}</div>}
-=======
                             </div>
                             <div className='col-md-7 col-12 d-flex flex-column p-3'>
                                 <h5 className="mb-4">GET IN TOUCH</h5>
@@ -140,7 +95,6 @@ const Retailer = () => {
                                                         <option value="other">Other</option>
                                                     </select>
                                                     {errors.option && <div className="form-error">{errors.option}</div>}
->>>>>>> fc55de6adc9584f71e629aecac86eba10b4d9993
 
                                                 </div>
 
@@ -193,17 +147,11 @@ const Retailer = () => {
                                 </Formik>
                             </div>
                         </div>
-<<<<<<< HEAD
-                        {/* </div> */}
-                    </Container>
-                    
-=======
                     </div>
->>>>>>> fc55de6adc9584f71e629aecac86eba10b4d9993
                 </Container>
 
             </Container>
-            {/* Second Section */}
+            {/* Second Section
             <div className='d-flex py-3 flex-column align-items-center justify-content-center'>
                 <h1 style={{ textAlign: 'center', padding: "10px" }}>Handover से जुड़ने के फायदें </h1>
                 <div className='row' style={{ width: "100%" }}>
@@ -263,49 +211,11 @@ const Retailer = () => {
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
-                {/* Statistics Section */}
-                <HandOverStatistics/>
-                {/* Careers */}
-                <Container  className="p-0 d-flex flex-column gap-2 align-items-center " style={{position:"relative"}}>
-                    <h3 className='mt-4 mb-4 fw-bold'>Careers @Handover</h3>
-                    <p>Besides offering a promising ‘Delivery Partner’ job, Handover, as a brand, has many more opportunities up for grabs. These could be in operations, finance, technology, etc. So, if you know how to deliver multiple tasks and are a problem-solver, we would like to talk to you! Send your resume now!</p>
-                     <div className='mt-2'>
-                        <div style={{width:"100%"}}>
-                            <img src="./career_banner.png" alt="Career Banner" style={{width:"100%"}}/>
-                        </div>
-                     </div>
-                     <div className="d-none d-md-block" style={{position:"absolute",bottom:"110px",left:"80px"}}> 
-                        <img src="./handover_logo.png" alt="HandOver Logo" width="200px"/>
-                     </div>
-                     <div className='align-self-start text-start' style={{marginLeft:"100px"}}> 
-                        <p style={{fontSize:"27px",color:"#4F4F4F"}}> Find Opportunties for you</p>
-                        <Button variant="primary">Explore Carrers</Button>
-                    </div>
-                    <div className='handover-shadow-div p-2 d-none d-md-block' 
-                    style={{width:"350px",position:"absolute",bottom:"30px",right:"100px"}}>
-                        <div className='d-flex flex-row gap-2'>
-                            <img src="./retailer_profile.png"alt="HandOver Profile" width="80px" height="80px"/>
-                            <div>
-                                <h5>WHAT OUR PEOPLE SAY</h5>
-                                <p>Lorem ipsum dolor sit amet .
-                                  The graphic and typographic operators know
-                                   this well, in reality.</p>
-                            </div>
-                        </div>
-                    </div>
-                </Container>
-                {/* Location Section */}
-                <Container fluid className="p-0 d-flex flex-column align-items-center justify-content-center">
-                    <h3 className='mt-4 mb-4 fw-bold'>Our Location</h3>
-                    <div className='d-flex flex-row flex-wrap gap-1 align-items-center justify-content-center pb-2'>
-=======
             </Container>
             {/* Location Section */}
             <Container fluid className="p-0 d-flex flex-column align-items-center justify-content-center">
                 <h3 className='mt-4 mb-4 fw-bold'>Our Location</h3>
                 <div className='d-flex flex-row flex-wrap gap-1 align-items-center justify-content-center pb-2'>
->>>>>>> fc55de6adc9584f71e629aecac86eba10b4d9993
                     {[1, 2, 3, 4, 5].map((item, index) => {
                         return (<LocationItem key={index} />)
                     })}
